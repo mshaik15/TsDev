bf0beb609ee713073d08204bbc110b65aeb9e950
zelkzayn <bhattiz@uoguelph.ca>
2025-07-24 17:13:09 -0400

    fixed data_cleaning logic

--- src/data_input.py
+++ src/data_input.py
@@ -1,32 +1,32 @@
 import pandas as pd
 import numpy as np
 
-def load_data_csv(file_path) -> pd.DataFrame:
+def load_data_csv(file_path) -> tuple[pd.DataFrame, str]:
     try:
         df = pd.read_csv(file_path)
         print(f"Data loaded successfully from {file_path}")
-        return df
+        return df, file_path
     
     except Exception as e:
         print(f"Error loading data from {file_path}: {e}")
-        return None
+        return None, None
 
-def load_data_json(file_path) -> pd.DataFrame:
+def load_data_json(file_path) -> tuple[pd.DataFrame, str]:
     try:
         df = pd.read_json(file_path)
         print(f"Data loaded successfully from {file_path}")
-        return df
+        return df, file_path
     
     except Exception as e:
         print(f"Error loading data from {file_path}: {e}")
-        return None
+        return None, None
 
-def load_data_excel(file_path) -> pd.DataFrame:
+def load_data_excel(file_path) -> tuple[pd.DataFrame, str]:
     try:
         df = pd.read_excel(file_path)
         print(f"Data loaded successfully from {file_path}")
-        return df
+        return df, file_path
     
     except Exception as e:
         print(f"Error loading data from {file_path}: {e}")
-        return None
+        return None, None
